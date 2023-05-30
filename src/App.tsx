029f0b21c352f55ee462e511a5b20d87268c066c
designyamah <nnyamahjoseph@gmail.com  git config --global user.name designyamahgit config --global user.email nnyamahjoseph@gmail.com>
2023-05-30 14:24:09 +0100

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <>
-      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-        <GridItem w="100%" h="60" bg="blue.500" colSpan={3} rowSpan={2}>
-          1
-        </GridItem>
-        <GridItem w="100%" h="10" bg="blue.500" colSpan={2}>
-          2
-        </GridItem>
-        <GridItem w="100%" h="10" bg="blue.500">
-          3
-        </GridItem>
-        <GridItem w="100%" h="10" bg="blue.500">
-          4
-        </GridItem>
-        <GridItem w="100%" h="10" bg="blue.500">
-          5
-        </GridItem>
-      </Grid>
       <Grid
         templateAreas={{
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`,
         }}
-        gap={2}
       >
-        <GridItem w="100%" bg="green" area={"nav"}>
-          1
+        <GridItem w="100%" area={"nav"}>
+          <Navbar />
         </GridItem>
         <Show above="lg">
           <GridItem w="100%" bg="blue.500" area={"aside"}>
