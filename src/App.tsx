60ec08bdd83d11dddf4d2c612783ba06603f370e
designyamah <nnyamahjoseph@gmail.com  git config --global user.name designyamahgit config --global user.email nnyamahjoseph@gmail.com>
2023-05-30 14:03:48 +0100

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+        <GridItem w="100%" h="60" bg="blue.500" colSpan={3} rowSpan={2}>
+          1
+        </GridItem>
+        <GridItem w="100%" h="10" bg="blue.500" colSpan={2}>
+          2
+        </GridItem>
+        <GridItem w="100%" h="10" bg="blue.500">
+          3
+        </GridItem>
+        <GridItem w="100%" h="10" bg="blue.500">
+          4
+        </GridItem>
+        <GridItem w="100%" h="10" bg="blue.500">
+          5
+        </GridItem>
+      </Grid>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        gap={2}
+      >
+        <GridItem w="100%" bg="green" area={"nav"}>
+          1
+        </GridItem>
+        <Show above="lg">
+          <GridItem w="100%" bg="blue.500" area={"aside"}>
+            2
+          </GridItem>
+        </Show>
+        <GridItem w="100%" bg="yellow" area={"main"}>
+          3
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
