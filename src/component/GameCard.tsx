46fdf1ddd8f7ff9192c99a578ef993f32950d9f2
designyamah <nnyamahjoseph@gmail.com  git config --global user.name designyamahgit config --global user.email nnyamahjoseph@gmail.com>
2023-07-09 20:11:55 +0100

    clean up the game card

--- src/component/GameCard.tsx
+++ src/component/GameCard.tsx
@@ -22,6 +22,12 @@ const GameCard = ({ game }: Props) => {
     <Card overflow={"hidden"} borderRadius={10}>
       <Image src={getCropUrl(game.background_image)} />
       <CardBody>
+        <HStack justifyContent="space-between">
+          <IconPlatForm
+            platform={game.parent_platforms.map((p) => p.platform)}
+          />
+          <Gamescore score={game.metacritic} />
+        </HStack>
         <Heading fontSize="2xl">{game.name}</Heading>
         {/* <List>
           {game.parent_platforms.map(({ platform }) => (
@@ -29,12 +35,6 @@ const GameCard = ({ game }: Props) => {
           ))}
         </List> */}
         {/* method 2 */}
-        <HStack justifyContent="space-between">
-          <IconPlatForm
-            platform={game.parent_platforms.map((p) => p.platform)}
-          />
-          <Gamescore score={game.metacritic} />
-        </HStack>
       </CardBody>
     </Card>
   );
